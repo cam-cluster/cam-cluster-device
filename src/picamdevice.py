9001f5a065889c3d99f18f2e814455f41800a537
Kevin Killingsworth <kevin@redfenix.com>
2019-07-08 00:38:27 -0500

    🚧  Add temporary base code for http server
    This adds base code for an http server and adds a service action to send
    http requests to the cluster server for periodic registration
    
    This code will mostly move to other areas

--- src/picamdevice.py
+++ src/picamdevice.py
@@ -15,7 +15,9 @@ __maintainer__ = "Kevin Killingsworth"
 __email__ = "kk@redfenix.com"
 __status__ = "Pre-alpha"
 
+from http import server
 from os import environ
+import socketserver
 import time
 
 from camclusterapi import CamClusterApi
@@ -24,12 +26,56 @@ from deviceinfo import DeviceInfo
 serverUrl = environ[ 'SERVER_URL' ]
 deviceName = environ[ 'DEVICE_NAME' ]
 registerInterval = int( environ[ 'REGISTER_INTERVAL' ] )
+camServerPort = int( environ[ 'CAM_SERVER_PORT' ] )
 
 print( 'Starting up "', deviceName, '"...' )
 
 deviceInfo = DeviceInfo( deviceName )
-camClusterApi = CamClusterApi( serverUrl )
 
-while True:
-	camClusterApi.register( deviceInfo )
-	time.sleep( registerInterval );
+class CamClusterClient( object ):
+	def __init__( self, registerInterval ):
+		self.registerInterval = registerInterval
+		self.camClusterApi = CamClusterApi( serverUrl )
+		self.lastRegister = 0
+		self.update()
+
+	def update( self ):
+		now = time.time()
+		if ( self.lastRegister + self.registerInterval <= now ):
+			self.camClusterApi.register( deviceInfo )
+			self.lastRegister = now
+
+client = CamClusterClient( registerInterval )
+
+class HTTPHandler( server.BaseHTTPRequestHandler ):
+	def do_GET( self ):
+		if self.path == '/':
+			self.send_response( 301 )
+			self.send_header( 'Location', '/index.html' )
+			self.end_headers()
+		elif self.path == '/index.html':
+			text = 'CamServer test page'
+			content = text.encode( 'utf-8' )
+			self.send_response( 200 )
+			self.send_header( 'Content-Type', 'text/html' )
+			self.send_header( 'Content-Length', len( content ) )
+			self.end_headers()
+			self.wfile.write( content )
+		else:
+			self.send_error( 404 )
+			self.end_headers()
+
+class CamServer( socketserver.ThreadingMixIn, server.HTTPServer ):
+	allow_reuse_address = True
+	daemon_threads = True
+
+	def service_actions( self ):
+		client.update()
+
+address = ( '', camServerPort )
+server = CamServer( address, HTTPHandler )
+server.serve_forever()
+
+#while True:
+#	camClusterApi.register( deviceInfo )
+#	#time.sleep( registerInterval );
