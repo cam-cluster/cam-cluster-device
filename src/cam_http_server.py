9cab46d0a189909e9c53f17bf355fb0c8fde003a
Kevin Killingsworth <kevin@redfenix.com>
2019-07-10 11:58:40 -0500

    ♻️  http server: better organized
    Split out the responses into their own functions.
    Added a dictionary for lookups

--- src/cam_http_server.py
+++ src/cam_http_server.py
@@ -4,33 +4,45 @@ from http import server
 import json
 import socketserver
 
-def generate_info_page( info ):
+def error_not_found( handler ):
+	handler.send_error( 404 )
+	handler.end_headers()
+
+def redirect_to_index( handler ):
+	handler.send_response( 301 )
+	handler.send_header( 'Location', '/index.html' )
+	handler.end_headers()
+
+def serve_info_page( handler, info ):
 	text = '<h1>' + info[ 'name' ] + '</h1>\n' 
 	text += '<table>\n'
 	text += '  <tr><th>mac:</th><td>' + info[ 'mac' ] + '</td></tr>\n'
 	text += '  <tr><th>ip:</th><td>' + info[ 'ip' ] + '</td></tr>\n'
 	text += '</table>\n'
-	return text.encode( 'utf-8' )
+	content = text.encode( 'utf-8' )
 
-def create_handler( device_info ):
+	handler.send_response( 200 )
+	handler.send_header( 'Content-Type', 'text/html' )
+	handler.send_header( 'Content-Length', len( content ) )
+	handler.end_headers()
+	handler.wfile.write( content )
+
+get_paths = {
+	'/': redirect_to_index,
+	'/index.html': serve_info_page,
+}
+
+def create_handler( device_info, get_paths ):
 	class HTTPHandler( server.BaseHTTPRequestHandler ):
 		dev_info = device_info
 
 		def do_GET( self ):
-			if self.path == '/':
-				self.send_response( 301 )
-				self.send_header( 'Location', '/index.html' )
-				self.end_headers()
-			elif self.path == '/index.html':
-				content = generate_info_page( self.dev_info.getInfo() )
-				self.send_response( 200 )
-				self.send_header( 'Content-Type', 'text/html' )
-				self.send_header( 'Content-Length', len( content ) )
-				self.end_headers()
-				self.wfile.write( content )
+			handle_get = get_paths[ self.path ]
+
+			if handle_get:
+				handle_get( self, self.dev_info.getInfo() )
 			else:
-				self.send_error( 404 )
-				self.end_headers()
+				error_not_found( self )
 
 	return HTTPHandler
 
@@ -44,7 +56,7 @@ class CamHTTPServer( socketserver.ThreadingMixIn, server.HTTPServer ):
 		self.allow_reuse_address = True
 
 		address = ( '', port )
-		super().__init__( address, create_handler( device_info ) )
+		super().__init__( address, create_handler( device_info, get_paths ) )
 
 	def service_actions( self ):
 		self.client.update()
